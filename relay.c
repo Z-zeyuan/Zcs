c2e5c07e325f5ae807b045af4a332253234b2da8
Z-zeyuan <103002832+Z-zeyuan@users.noreply.github.com>
2024-03-03 21:18:21 -0500

    relay stuct

--- relay.c
+++ relay.c
@@ -68,10 +68,25 @@ pthread_t Lan2ServiceListenerThread;
 
 char *discovery = "D";
 
-void *TransitThread(mcast_t *From,mcast_t *To ){
-    multicast_setup_recv(Lan1_App_Service_Receive);
-    char msg[MAX_MSG_LENGTH];
-    while(join_threads == 0){
+/* Send msg to channel 'wave' times. */
+void send_msg(mcast_t *channel, void *msg, int msglen, int wave){
+    for (int i = 0; i < wave; i++){
+        int err = multicast_send(channel,msg,msglen);
+        //printf("Message Send: %d\n",err);
+        usleep(10000);
+    }
+}
+
+void *TransitThread(void *arg){
+    //arg/2 is index 
+    //arg//2 : if 0 APP if 1 Server listener
+    int index;
+    int IsApp;
+    if (IsApp == 0)
+    {
+        multicast_setup_recv(Lan1_App_Service_Receive);
+        char msg[MAX_MSG_LENGTH];
+        while(join_threads == 0){
         //printf("ServiceListener is alive...\n");
         memset(msg,'\0',sizeof(msg));
         while(multicast_check_receive(Lan1_App_Service_Receive) == 0){
@@ -83,6 +98,25 @@ void *TransitThread(mcast_t *From,mcast_t *To ){
         send_msg(Lan2_Service_App_Send,msg,MAX_MSG_LENGTH,1);
         
     }
+    }
+    else
+    {
+        multicast_setup_recv(Lan1_App_Service_Receive);
+        char msg[MAX_MSG_LENGTH];
+        while(join_threads == 0){
+        //printf("ServiceListener is alive...\n");
+        memset(msg,'\0',sizeof(msg));
+        while(multicast_check_receive(Lan1_App_Service_Receive) == 0){
+            //printf("Service is waiting for message...\n");
+        }
+        multicast_receive(Lan1_App_Service_Receive,msg,MAX_MSG_LENGTH);
+        printf("App's new msg: %s\n",msg);
+        
+        send_msg(Lan2_Service_App_Send,msg,MAX_MSG_LENGTH,1);
+    }
+    }
+    
+    
 }
 
 
@@ -172,15 +206,21 @@ int relay_init(){
     return 0;
 }
 
-// void SetupSockets(){
-//     for (int i = 0; i < 2; i++)
-//     {
-//         RelayTable[i].sockets[0]=
-//         RelayTable[i].sockets[1]=
-//         RelayTable[i].sockets[2]=
-//         RelayTable[i].sockets[3]=
-//     }
-// }
+void SetupSockets(int index,char* ips[],int AppS,int AppR , int ServS, int ServR){
+    //ips len 2 appip,serverIp
+    RelayTable[index].sockets[0]=multicast_init(ips[0],APP_SEND_PORT,DEFAULT_PORT);
+    RelayTable[index].sockets[1]=multicast_init(ips[0],DEFAULT_PORT,SERVICE_SEND_PORT);
+    RelayTable[i].sockets[2]=multicast_init(ips[1],SERVICE_SEND_PORT,DEFAULT_PORT);
+    RelayTable[i].sockets[3]=multicast_init(ips[1],DEFAULT_PORT,APP_SEND_PORT);
+    
+}
+
+void StartThread(int index){
+    //ips len 2 appip,serverIp
+    pthread_create(&TransitThread,NULL,Lan1AppListener,NULL);
+    pthread_create(&TransitThread,NULL,Lan1ServiceListener,NULL);
+    send_msg(Lan1_Service_App_Send,discovery,strlen(discovery),3);
+}
 
 int relay_up(){
     // for (int i = 0; i < 2; i++)
@@ -194,13 +234,26 @@ int relay_up(){
     pthread_create(&Lan1ServiceListenerThread,NULL,Lan1ServiceListener,NULL);
     pthread_create(&Lan2AppListenerThread,NULL,Lan2AppListener,NULL);
     pthread_create(&Lan2ServiceListenerThread,NULL,Lan2ServiceListener,NULL);
-    
+
+    send_msg(Lan1_Service_App_Send,discovery,strlen(discovery),3);
+    send_msg(Lan2_Service_App_Send,discovery,strlen(discovery),3);
     return 0;
 }
 
 
 int relay_down(int type){
     join_threads=1;
-
     return 0;
+}
+
+
+
+int main(){
+    //input?
+
+    int a = relay_init();
+    a=relay_up();
+    sleep(300);
+    a=relay_down();
+    return a;
 }
